e39c28e5af228b33d54c65eb73661d7fd0d0ce31
ClaraCastaD <1980@holbertonschool.com>
2020-12-15 16:25:42 +0000

    Fixing user.py

--- models/user.py
+++ models/user.py
@@ -6,6 +6,10 @@ from sqlalchemy import Column, String
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    email = email address
+    password = password to login
+    first_name = first name
+    last_name = last name
     __tablename__ = 'users'
     email = Column(String(128), nullable = False)
     password = Column(String(128), nullable = False)
