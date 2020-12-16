69d2aadf9de94e707fe5253631a615d00732ece2
idelahoz14 <idelahoz14@cuc.edu.co>
2020-12-16 03:55:31 +0000

    fixing code(pep8 and adding relationship)

--- models/user.py
+++ models/user.py
@@ -5,10 +5,13 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
 
+
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
     email = Column(String(128), nullable = False)
     password = Column(String(128), nullable = False)
-    first_name =  Column(String(128))
-    last_name =  Column(String(128))
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+
+    places = relationship('Place', cascade='delete', backref='user')
