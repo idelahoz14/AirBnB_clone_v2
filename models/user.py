e67b47ae10f3c01310ccb91e01dd5bc8be9cf289
idelahoz14 <idelahoz14@cuc.edu.co>
2020-12-16 03:41:41 +0000

    fixing code(import new module and remove constructor)

--- models/user.py
+++ models/user.py
@@ -2,16 +2,13 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-
+from sqlalchemy.orm import relationship
+from os import getenv
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
     email = Column(String(128), nullable = False)
     password = Column(String(128), nullable = False)
-    first_name =  Column(String(128), nullable = False)
-    last_name =  Column(String(128), nullable = False)
-
-    def __init__(self, *args, **kwargs):
-        """ User constructor """
-        super().__init__(*args, **kwargs)
+    first_name =  Column(String(128))
+    last_name =  Column(String(128))
