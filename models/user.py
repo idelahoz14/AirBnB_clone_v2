231df5628be86747d34243078973809fe29c9d58
ClaraCastaD <1980@holbertonschool.com>
2020-12-17 03:35:41 +0000

    Fixing mistake in user.py

--- models/user.py
+++ models/user.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.place import Place
-from os import getenv
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
+class User(BaseModel):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    email = Column(String(128), nullable = False)
-    password = Column(String(128), nullable = False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-
-    places = relationship('Place', cascade='delete', backref='user')
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
