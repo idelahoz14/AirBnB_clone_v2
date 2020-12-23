e75e431129439646a41ec2bff12be6e97fdeedea
idelahoz14 <idelahoz14@cuc.edu.co>
2020-12-23 01:09:42 +0000

    task number 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Script that sets up your web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get install nginx
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
+sudo service nginx restart
