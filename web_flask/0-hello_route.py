19a33c28f06d5f2e84a38bba545fd7c284405493
idelahoz14 <idelahoz14@cuc.edu.co>
2021-01-19 17:23:36 +0000

    fixing code(pep8 style)

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Starts a flask web application """
 from flask import Flask
-
-
 app = Flask(__name__)
+
+
 @app.route('/' strict_slashes=False)
 def hello_HBNH():
     """ Display Hello HBNB! """
