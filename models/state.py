b3905d2a89bc8d3685cb7d2683c5c5263aa8efd3
idelahoz14 <idelahoz14@cuc.edu.co>
2021-01-20 21:49:32 +0000

    fixing code(pep8 style)

--- models/state.py
+++ models/state.py
@@ -12,15 +12,17 @@ from models import storage
 class State(BaseModel):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable = False)
+    name = Column(String(128), nullable=False)
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", backref='state', cascade="all, delete, delete-orphan")
+        cities = relationship("City",
+                              backref='state',
+                              cascade="all, delete, delete-orphan")
     else:
         @property
         def cities(self):
             """ List the cities """
             List = []
             for key, value in storage.all(City).items():
-            if self.id == obj.state_id:
-                List.append(value)
+                if self.id == obj.state_id:
+                    List.append(value)
         return List
